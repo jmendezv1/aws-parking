39608acb49b65cc95c5dbda14657390baaf35b57
Javier Mendez <jmendezva89@gmail.com>
2022-07-10 01:40:03 -0500

    Se agrego el auto token en cada usuario

--- db/db.sql
+++ db/db.sql
@@ -13,3 +13,20 @@ CREATE TABLE users(
     created_at TIMESTAMP(0) NOT NULL,
     updated_at TIMESTAMP(0) NOT NULL
 ); 
+
+CREATE TABLE parking_place(
+    id BIGSERIAL PRIMARY KEY,
+    name_place VARCHAR(255) NOT NULL,
+    description_place VARCHAR(255) NULL,
+    u_user_plate VARCHAR(50) NULL UNIQUE,
+    u_user_name VARCHAR(80) NULL,
+    u_user_phone VARCHAR(80) NOT NULL UNIQUE,
+    u_user_img VARCHAR(80) NULL,
+    is_available BOOLEAN NULL,
+    session_token VARCHAR (255) NULL,
+    place_Lat VARCHAR(255) NULL NULL UNIQUE,
+    place_Log VARCHAR(255) NULL NULL UNIQUE,
+    parking_Lot VARCHAR(255) NOT NULL
+); 
+
+
