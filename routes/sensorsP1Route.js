bc42ae5e1f4ccbc0164e9ac18dafca70d75e49d6
Javier Mendez <jmendezva89@gmail.com>
2022-07-14 17:37:40 -0500

    ffff sensor

---
+++ routes/sensorsP1Route.js
@@ -0,0 +1,11 @@
+const SensorP1Controller = require('../controllers/sensorsController');
+
+module.exports = (app) => {
+
+     //TODO: Parqueadero 1
+    //Get : Obtener datos
+    app.get('/api/parking1_sensors/getAll',SensorP1Controller.getAll);
+    //Post: Ingresr datos
+    app.post('/api/parking1_sensors/create',SensorP1Controller.register);
+
+}
